0435b966894c29db45cf64a2a5465dcee8f56b7e
Rustam Khan <kh4n2809@gmail.com>
2023-12-27 10:10:54 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,36 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader";
 
-{ /* default Route */ }
-const PageNotFound = lazy(() => import("./components/PageNotFound"));
+{
+  /* default Route */
+}
+const PageNotFound = lazy(() => import("../PageNotFound.tsx"));
 
-{ /* Pages */ }
+{
+  /* Pages */
+}
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Customers = lazy(() => import("./pages/Customers"));
 const Products = lazy(() => import("./pages/Products"));
 const Transactions = lazy(() => import("./pages/Transactions"));
 
-{ /* charts */ }
+{
+  /* charts */
+}
 const BarCharts = lazy(() => import("./pages/charts/BarCharts"));
 const PieCharts = lazy(() => import("./pages/charts/PieCharts"));
 const LineCharts = lazy(() => import("./pages/charts/LineCharts"));
 
-{ /* apps */ }
+{
+  /* apps */
+}
 const Stopwatch = lazy(() => import("./pages/app/Stopwatch.tsx"));
 const Coupon = lazy(() => import("./pages/app/Coupon.tsx"));
 const Toss = lazy(() => import("./pages/app/Toss.tsx"));
 
-{ /* managements */ }
+{
+  /* managements */
+}
 const NewProduct = lazy(() => import("./pages/managements/NewProduct"));
 const ProductManagement = lazy(
   () => import("./pages/managements/ProductManagement")
@@ -35,11 +45,10 @@ const App = () => {
     <Router>
       <Suspense fallback={<Loader />}>
         <Routes>
+          {/* default Route */}
+          <Route path="*" element={<PageNotFound />} />
 
-        { /* default Route */ }
-          <Route path="/" element={<PageNotFound />} />
-
-        { /* Pages */ }
+          {/* Pages */}
           <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/customers" element={<Customers />} />
           <Route path="/admin/products" element={<Products />} />
